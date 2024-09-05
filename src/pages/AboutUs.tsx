1ec51f36427845d492e8de8bd9339a43c035a9d9
Rohit Verma <137288929+Roh17v@users.noreply.github.com>
2024-09-05 17:50:56 +0000

    refactor: Removed unnecessary imports

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,6 +1,3 @@
-import React from "react";
-import Navbar from "../components/Navbar";
-
 const AboutUs = () => {
   return <div>About Us</div>;
 };
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,6 +1,3 @@
-import React from "react";
-import Navbar from "../components/Navbar";
-
 const Contact = () => {
   return <div>contact</div>;
 };
